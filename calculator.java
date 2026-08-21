ef93c64be5328879e2232898af37b3f04f7dd7be
Tamanna Jangra <tamannajangra55@gmail.com>
2026-08-21 16:48:12 +0530

    calculator +-*/

---
+++ calculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;//import scanner calss to read user input
+
+public class calculator {
+   public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    System.out.println("give me your first number : ");
+    int i=input.nextInt();
+    System.out.println("choose an operator(+,-,*,/)");
+    char operator=input.next().charAt(0);
+    System.out.println("give me your second number: ");
+    int j=input.nextInt();
+
+    double result;
+    switch(operator){
+        case '+':
+            result=i+j;
+            break;
+        
+        case '-':
+             result=i-j;
+             break;
+        case'*' :
+             result=i*j;
+             break;
+
+        case'/':
+            if(j==0){
+                System.out.println("Error: Division by zero is not allowed.");
+                input.close();
+                return;
+            }
+        result=i/j;
+        break;
+
+        default: 
+            System.out.println("Invalid operator entered.");
+            input.close();
+            return;
+    }
+
+System.out.println(" Result: "+ i +" "+ operator+ " "+ j+ " "+ "="+" "+ result);
+input.close();
+   } 
+}
